695e168014ce4ea2e9d11073374f714a70b72a08
John Doe <mitchellcaleb00@gmail.com>
2022-01-20 04:15:51 -0800

    show WPForm database

--- displaylist.php
+++ displaylist.php
@@ -2,6 +2,7 @@
 
 global $wpdb;
 $tablename = $wpdb->prefix."udmanage";
+define( 'DMANGE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
 // Delete record
 if(isset($_GET['delid'])){
@@ -9,14 +10,32 @@ if(isset($_GET['delid'])){
 	$wpdb->query("DELETE FROM ".$tablename." WHERE id=".$delid);
 }
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="http://use.fontawesome.com/releases/v5.13.0/css/all.css">
 <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"> -->
-<link rel="stylesheet" href="<?php echo plugins_url('/udmanage/css/main.css'); ?>" />
+<link rel="stylesheet" href="<?php echo esc_url( plugins_url( '/css/main.css', __FILE__ ) ) ?>" crossorigin="anonymous" >
 
 
 <h1>Clients contact table </h1>
 <div class="udmanage">
+	<?php
+		// $entriesList = $wpdb->get_results("SELECT * FROM npg_wpforms_entries");
+		// if(count($entriesList) > 0){
+		// 	foreach($entriesList as $entry){
+		// 		print_r($entry->fields);
+		// 		echo '<br>';
+		// 	}
+		// }
+		// exit;
+
+		// $entriesList = $wpdb->get_results("SELECT * FROM npg_wpforms_entry_fields LIMIT 100 WHERE form_id = 197");
+		// if(count($entriesList) > 0){
+		// 	foreach($entriesList as $entry){
+		// 		echo $entry->id.", ".$entry->entry_id.", ".$entry->form_id.", ".$entry->field_id.", ".$entry->value.'<br>';
+		// 	}
+		// }
+		// exit;
+	?>
 	<table id = "usertable" class="table table-bordered table-striped table-hover table-sm">
 		<thead class="table-info">
 			<tr class="title">
@@ -27,23 +46,36 @@ if(isset($_GET['delid'])){
 				<th>Use in 7days</th>
 				<th>Star rate</th>
 				<th>Created in</th>
-				<th>&nbsp;</th>
 			</tr>
 		</thead>
 		<tbody>
 		<?php
 		// Select records
-		$entriesList = $wpdb->get_results("SELECT * FROM ".$tablename." order by modify_date");
+		$entriesList = $wpdb->get_results("SELECT * FROM npg_wpforms_entry_fields");
 		if(count($entriesList) > 0){
 			$count = 1;
+			$startVal = 0;
+			$userArray = array();
+			$record = array();
 			foreach($entriesList as $entry){
-				$id = $entry->id;
-				$name = $entry->user_name;
-				$gmail = $entry->user_mail;
-				$amazone_id = $entry->amazon_id;
-				$usein = $entry->usein_7day;
-				$starrate = $entry->star_rate;
-				$datetime = $entry->modify_date;
+				$field_id = $entry->field_id;
+				if ($startVal > 5)
+				{
+					$startVal = 0;
+					array_push($userArray, $record);
+					$record = array();
+				}
+				array_push($record,$entry->value);
+				$startVal++;
+			}
+			foreach($userArray as $entry){
+				//$id = $entry->id;
+				$name = $entry[0];
+				$gmail = $entry[1];
+				$amazone_id = $entry[2];
+				$usein = $entry[3];
+				$starrate = $entry[4];
+				$datetime = $entry[5];
 
 				$usein = $usein >= 1 ? "<i class='fas fa-shopping-cart'></i>" : " ";
 				echo "<tr>
@@ -62,9 +94,10 @@ if(isset($_GET['delid'])){
 					}
 				echo "</td>
 					<td>".$datetime."</td>
-					<td class='delete'><a num='".$id."' class='delete hover-white' alt='Click here to delete record.'><i class='fas fa-times' ></i></a></td>
+					
 				</tr>
 				";
+				//<td class='delete'><a num='".$id."' class='delete hover-white' alt='Click here to delete record.'><i class='fas fa-times' ></i></a></td>
 				$count++;
 			}
 		}else{
@@ -74,9 +107,9 @@ if(isset($_GET['delid'])){
 		</tbody>
 	</table>
 </div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="<?php echo plugins_url('/udmanage/plugins/jquery-3.6.0.min.js'); ?>"></script>
-<script src="<?php echo plugins_url('/udmanage/plugins/fancyTable.min.js'); ?>"></script>
-<script src="<?php echo plugins_url('/udmanage/js/template.js'); ?>"></script>
+<script src="http://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="<?php echo esc_url( plugins_url( '/plugins/jquery-3.6.0.min.js', __FILE__ ) ) ?>" crossorigin="anonymous"></script>
+<script src="<?php echo esc_url( plugins_url( '/plugins/fancyTable.js', __FILE__ ) ); ?>"></script>
+<script src="<?php echo esc_url( plugins_url( '/js/template.js', __FILE__ ) ); ?>"></script>
--- udmanage.php
+++ udmanage.php
@@ -31,9 +31,9 @@ register_activation_hook( __FILE__, 'udmanage_table' );
 // Add menu
 function udmanage_menu() {
 
-    add_menu_page("User Data Manage", "User Data Manage","manage_options", "myplugin", "displayList",plugins_url('/udmanage/img/icon.png'));
+    add_menu_page("User Data Manage", "User Data Manage","manage_options", "myplugin", "displayList",esc_url( plugins_url( '/img/icon.png', __FILE__ ) ) );
     add_submenu_page("myplugin","User Data", "User Data","manage_options", "allusers", "displayList");
-    add_submenu_page("myplugin","Add new Data", "Add new Data","manage_options", "addnewdata", "addEntry");
+    //add_submenu_page("myplugin","Add new Data", "Add new Data","manage_options", "addnewdata", "addEntry");
 }
 
 add_action("admin_menu", "udmanage_menu");
